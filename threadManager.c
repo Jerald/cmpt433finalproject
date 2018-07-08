62a4c8e7021870f785ecd01415345bb47f838ae2
Benjamin Wiens <benjamin.s.wiens@gmail.com>
2018-07-08 15:09:15 -0700

    updated makefile, removed controller_src file

--- threadManager.c
+++ threadManager.c
@@ -1,8 +1,8 @@
 #include <pthread.h>
 #include "machineInterface.h"
 
-void ThreadManager_init();
+void ThreadManager_init()
 {
-	MachineInterface_init();
+	//MachineInterface_init();
 	//start threads for modules here.
 }
