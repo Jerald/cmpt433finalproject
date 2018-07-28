d7d13c8cb88bbdfbf3ed0a52c8f75bf3e35b3390
atena <atena.savija@gmail.com>
2018-07-28 12:22:01 -0700

    fixed timestamp issue

--- setup.sql
+++ setup.sql
@@ -2,30 +2,17 @@
 
 
 CREATE TABLE Drinks (
-  col_num integer NOT NULL CHECK (col_num >= 1 AND col_num <= 8),
+  col_num integer NOT NULL CHECK (col_num >= 0 AND col_num <= 7),
   count integer NOT NULL CHECK (count >= 0),
   PRIMARY KEY(col_num)
 );
 
 
 CREATE TABLE Purchases (
-  col_num integer CHECK (col_num >= 1 AND col_num <=8),
-  purchase_date timestamp
+  col_num integer CHECK (col_num >= 0 AND col_num <= 7),
+  purchase_date timestamp default current_timestamp
 );
 
 
-
-CREATE FUNCTION update_purchase_timestamp() RETURNS trigger
-    LANGUAGE plpgsql
-    AS $$
-  BEGIN
-    NEW.purchase_date = NOW();
-    RETURN NEW;
-  END;
-$$;
-
-CREATE TRIGGER t1_purchase BEFORE UPDATE ON Purchases FOR EACH ROW EXECUTE PROCEDURE update_purchase_timestamp();
-
-
 INSERT INTO Drinks VALUES
-  (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0);
+  (0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0);
