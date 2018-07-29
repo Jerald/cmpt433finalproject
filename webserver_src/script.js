ef3cd4741462ca02c9dc3efc2b30ca3ea506e987
Oscar <osmithsi@sfu.ca>
2018-07-28 23:25:37 -0700

    Cleaned up client-side js script

--- webserver_src/script.js
+++ webserver_src/script.js
@@ -1,50 +1,21 @@
 "use strict";
 
+$(document).ready(init);
 
+function init()
+{
+    $.ajax({
+        url: "/getGraph",
+        dataType: "json",
+        method: "POST",
+        timeout: 5000,
+        success: graphAjaxSuccess,
+        error: (xhr, status, errorThrown) => console.log(`Status: ${status}\nError thrown: ${errorThrown}`)
+    });
+}
 
-
-
-
-$(document).ready(function() {
-	
-
-	/*
-	var net = require('net');
-	var client = new net.Socket();
-	
-	client.connect(12345, '192.168.7.2', function() {
-		console.log('Connected');
-		client.write('Message to Server: HI.');
-		
-		
-	});
-
-	client.on('data', function(data) {
-		console.log('Received: ' + data);
-		
-		//client.destroy(); // kill client after server's response
-	});
-
-	client.on('close', function() {
-		console.log('Connection closed');
-	});
-	*/
-	
-	
-	/*
-	 * //code found on: http://darrenoneill.co.uk/post/real-time-web-apps-postgresql-and-node/
-//WEB_SOCKET_SWF_LOCATION = 'inc/WebSocketMain.swf';
-var socket = io.connect('http://localhost:8088');
-socket.on('connected', function (data) {
-    socket.emit('ready for data', {});
-});
-
-socket.on('update', function (data) {
-    console.log(data.message.payload);
-});
-
-
-*/
-	
-	
-});
+function graphAjaxSuccess(data)
+{
+    $("#graph").attr("src", data.src);
+    $("#graph").attr("hidden", false);
+}
