46a3134074920008702f4900fb8a77e4752f7405
Benjamin Wiens <benjamin.s.wiens@gmail.com>
2018-07-28 19:05:08 -0700

    some javascripts and html files in the works

--- webserver_src/script.js
+++ webserver_src/script.js
@@ -1,20 +1,50 @@
 "use strict";
 
-//code found on: http://darrenoneill.co.uk/post/real-time-web-apps-postgresql-and-node/
+
+
+
+
 
 $(document).ready(function() {
 	
-WEB_SOCKET_SWF_LOCATION = 'inc/WebSocketMain.swf';
-var socket = io.connect('http://localhost:9000');
+
+	/*
+	var net = require('net');
+	var client = new net.Socket();
+	
+	client.connect(12345, '192.168.7.2', function() {
+		console.log('Connected');
+		client.write('Message to Server: HI.');
+		
+		
+	});
+
+	client.on('data', function(data) {
+		console.log('Received: ' + data);
+		
+		//client.destroy(); // kill client after server's response
+	});
+
+	client.on('close', function() {
+		console.log('Connection closed');
+	});
+	*/
+	
+	
+	/*
+	 * //code found on: http://darrenoneill.co.uk/post/real-time-web-apps-postgresql-and-node/
+//WEB_SOCKET_SWF_LOCATION = 'inc/WebSocketMain.swf';
+var socket = io.connect('http://localhost:8088');
 socket.on('connected', function (data) {
     socket.emit('ready for data', {});
 });
+
 socket.on('update', function (data) {
     console.log(data.message.payload);
 });
 
 
-
+*/
 	
 	
 });
--- webserver_src/server.js
+++ webserver_src/server.js
@@ -2,28 +2,70 @@
 
 const express = require('express');
 const fs   = require('fs');
+const http = require('http');
+const path = require('path');
 
 const SERVER_PORT = 8088;
 const server = express();
+const API_KEY = "otgY8vs1oqo2wXKYoiNY";
+const PLOTLY_USERNAME = "boat";
+var plotly = require('plotly')(PLOTLY_USERNAME, API_KEY);
+//var app = module.exports.app = express();
+//var server2 = http.createServer(app);
+//var tcpServer = require('./lib/tcp_server');
 
 // Helper functions
+
 function fileGetFunc(fileName, contentType)
 {
     return (req, res) =>
         res.sendFile(fileName, {root: __dirname, headers: {"Content-Type": contentType}});
 }
 
+
+function displayGraph()
+{
+	var data = [{x:[0,1,2], y:[3,2,1], type: 'bar'}];
+	var layout = {fileopt : "overwrite", filename : "simple-node-example"};
+	var url = '';
+	plotly.plot(data, layout, function (err, msg) {
+		if (err) return console.log(err);
+		//console.log(msg);
+		//JSON.stringify(msg);
+		//console.log(msg);
+		//console.log(msg.url);
+		//console.log(JSON.stringify(msg));
+		url = msg.url;
+		var start = url.indexOf('//');
+		var suburl = url.slice(start);	
+		var newsrc = suburl.concat(".embed");
+		
+		$('#graph').attr('src', newsrc);
+		//$('#graph').attr('width', 800);
+		//$('#graph').attr('height', 900);
+		$('#graph').attr('hidden', false);
+		
+		
+		//width:0; height:0; border:0; border:none"
+		console.log(newsrc);
+		
+	});
+}
+
 // Server routing functions
-server.get("/style.css", fileGetFunc("style.css", "text/css"));
+//server.get("/style.css", fileGetFunc("style.css", "text/css"));
 server.get("/script.js", fileGetFunc("script.js", "application/js"));
 
 server.get("/", function (req, res)
 {
-    fileGetFunc("index.html", "text/html");
+    //fileGetFunc("/index.html", "text/html");
+	res.sendFile(path.join(__dirname + '/index.html'));
+	displayGraph();	
 });
 
-server.listen(PORT_NUMBER, function ()
+server.listen(SERVER_PORT, function ()
 {
-	console.log("Server listeneing on port " + PORT_NUMBER);
+	console.log("Server listeneing on port " + SERVER_PORT);
 });
 
+//tcpServer.listen(server2);
