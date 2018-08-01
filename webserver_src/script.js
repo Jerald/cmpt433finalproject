e6839d09698a370f02d3452376143ac04c119cb5
Benjamin Wiens <benjamin.s.wiens@gmail.com>
2018-08-01 04:02:02 -0700

    Fixed plotly (unfinished but WAYYY better)

--- webserver_src/script.js
+++ webserver_src/script.js
@@ -5,15 +5,55 @@ $(document).ready(init);
 // How often it asks for an update from the database, in ms
 const UPDATE_RATE = 10000;
 var popUpdateInterval;
+const GRAPH_DIV_ID = "graph";
+const HOUR_STARTS = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
+var bname1 = "Coke";
+var bname2 = "Water";
+var bname3 = "Sugarless Mystery";
+var bname4 = "Canada Dry";
+var bname5 = "Nestea";
+var bname6 = "Minute Maid";
+var bname7 = "Monster";
+var bname8 = "Mystery";
+
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+});
+
+
+$('#reset').click(function(){
+	//remove db tables and remake
+	
+	$('#asofdate').text(new Date().toDateInputValue());
+	
+});
+
+$('#datepicker').change(function(){
+	$.ajax({
+        url: "/getGraph",
+        dataType: "json",
+        data: JSON.stringify({date: $('datepicker').val}),
+        method: "POST",
+        timeout: 50000,
+        success: updateGraphAjaxSuccess,
+        error: (xhr, status, errorThrown) => console.log(`Graph get error\nStatus: ${status}\nError thrown: ${errorThrown}`)
+    });
+});
 
 function init()
 {
+	$('#datePicker').val(new Date().toDateInputValue()); //WHY NO SET?
+	
+	//TODO query for asofdate (order by timestamp descending and pick first row)
+	
     $.ajax({
         url: "/getGraph",
         dataType: "json",
         method: "POST",
         timeout: 50000,
-        success: graphAjaxSuccess,
+        success: createGraphAjaxSuccess,
         error: (xhr, status, errorThrown) => console.log(`Graph get error\nStatus: ${status}\nError thrown: ${errorThrown}`)
     });
 
@@ -24,39 +64,41 @@ function init()
 	popUpdateGet();
 }
 
-function graphAjaxSuccess(data)
+function initGraph(data)
 {
-    // Set the new graph url in the iframe and make it visible
-    
-	/*
-	$("#graph").attr("src", data.src);
-    $("#graph").attr("hidden", false);
-    */
-    
-	$("#graph1").attr("src", data.src[0]);
-	$("#graph1").attr("hidden", false);
+	var graphType = "scatter";
+	var trace1 = {x: HOUR_STARTS, y: data[0], type: graphType, name: bname1};
+	var trace2 = {x: HOUR_STARTS, y: data[1], type: graphType, name: bname2};
+	var trace3 = {x: HOUR_STARTS, y: data[2], type: graphType, name: bname3};
+	var trace4 = {x: HOUR_STARTS, y: data[3], type: graphType, name: bname4};
+	var trace5 = {x: HOUR_STARTS, y: data[4], type: graphType, name: bname5};
+	var trace6 = {x: HOUR_STARTS, y: data[5], type: graphType, name: bname6};
+	var trace7 = {x: HOUR_STARTS, y: data[6], type: graphType, name: bname7};
+	var trace8 = {x: HOUR_STARTS, y: data[7], type: graphType, name: bname8};
 	
-	$("#graph2").attr("src", data.src[1]);
-	$("#graph2").attr("hidden", false);
+	var yaxis_template = {title: "STARTING HOUR"};
+	var xaxis_template = {title: "PURCHASES"};
 	
-	$("#graph3").attr("src", data.src[2]);
-	$("#graph3").attr("hidden", false);
 	
-	$("#graph4").attr("src", data.src[3]);
-	$("#graph4").attr("hidden", false);
-	
-	$("#graph5").attr("src", data.src[4]);
-	$("#graph5").attr("hidden", false);
-	
-	$("#graph6").attr("src", data.src[5]);
-	$("#graph6").attr("hidden", false);
-	
-	$("#graph7").attr("src", data.src[6]);
-	$("#graph7").attr("hidden", false);
-	
-	$("#graph8").attr("src", data.src[7]);
-	$("#graph8").attr("hidden", false);
-    
+	var graphData = [trace1, trace2, trace3, trace4, trace5, trace6, trace7, trace8];
+	var graphLayout = {showlegend: "true", title: "HOURLY NUMBER OF DRINK PURCHASES", 
+			xaxis: xaxis_template, yaxis: yaxis_template}; 
+	var graphConfig = {};
+	var fig = {data: graphData, layout: graphLayout, config: graphConfig};
+	return fig;
+}
+
+function updateGraphAjaxSuccess(data)
+{
+	var figure = initGraph(data);
+	Plotly.update(GRAPH_DIV_ID,figure);
+}
+
+function createGraphAjaxSuccess(data)
+{
+	var figure = initGraph(data);
+	Plotly.newPlot(GRAPH_DIV_ID,figure);
+	//Plotly.newPlot(graphDiv, graphData, graphLayout, graphConfig);
 }
 
 function popUpdateGet()
@@ -78,7 +120,6 @@ function popUpdateSuccess(data)
     // There are never EVER going to be more than 8 buttons on this system...
     for (var i = 1; i <= 8; i++)
     {
-        // console.log(`$(\`#button${i}Count\`).text(data[\`button${i}\`].count);`)
         // Set the count column of each button's row to the count value we got
         $(`#button${i}Count`).text(data[`button${i}`].count);
     }
--- webserver_src/server.js
+++ webserver_src/server.js
@@ -10,8 +10,8 @@ const { Client } = require('pg')
 const SERVER_PORT = 8088;
 const server = express();
 
-const API_KEY = "otgY8vs1oqo2wXKYoiNY";
-const PLOTLY_USERNAME = "boat";
+const API_KEY = "6Zn0UN6Uzcwdes3BuiOu";
+const PLOTLY_USERNAME = "Jeraldson";
 var plotly = require('plotly')(PLOTLY_USERNAME, API_KEY);
 var plotUrl = "";
 
@@ -25,9 +25,9 @@ const psqlClient = new Client({
 psqlClient.connect();
 
 
-function makeButtonGraphs(res)
-{
-	var hour_starts = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,29,20,21,22,23];
+function getGraphDataPoints(res)
+{	
+	console.log(res);
 	var button1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 	var button2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 	var button3 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
@@ -36,39 +36,48 @@ function makeButtonGraphs(res)
 	var button6 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 	var button7 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 	var button8 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+	var buttons = [button1, button2, button3, button4, button5, button6, button7, button8];
 	
 	//var date = "2018-07-29"; //temporary
 	//if no work, change second DD to ZZ and replace YYYY-MM-ZZ with Date + 1
-	var defQuery = "SELECT col_num, EXTRACT(HOUR FROM purchase_date) AS HOUR_START, count(EXTRACT(HOUR FROM purchase_date)) FROM purchases WHERE purchase_date BETWEEN '2018-07-30 00:00:00' AND '2018-07-31 00:00:00' group by HOUR_START, col_num order by col_num, HOUR_START asc";
+	var defQuery = "SELECT col_num, EXTRACT(HOUR FROM purchase_date) AS HOUR_START, count(EXTRACT(HOUR FROM purchase_date)) FROM purchases WHERE purchase_date BETWEEN '2018-07-29 00:00:00' AND '2018-07-30 00:00:00' group by HOUR_START, col_num order by col_num, HOUR_START asc";
 	//var newquery = defQuery.replace("YYYY-MM-DD", date);
 	
 	//console.log(newquery);
-	console.log(defQuery);
+	//console.log(defQuery);
 	psqlClient.query(defQuery, function (err, response)
 	{
-		//console.log(newquery);
+		console.log(response.rows);
         if (err)
         {
             console.log(`Error in database query for buttons\nError is: ${err}`);
-            res.end(500);
+            res.end();
             return;
         }
-        var dbrow = 0; 
-    
+        else if (response.rows === undefined || response.rows.length == 0) 
+		{
+			console.log("query returned no rows\n");
+			res.end();
+			return;
+		}
+     
+        var dbrow = 0;  //if only one row returned, the row is dbrow0 and is of length 1
+        var numRows = response.rows.length;
         for(var i = 0; i <= 7; i++) //each col_n
         {
-        	for(var j = 0; i <= 23; i++)//24 hours
+        	if(dbrow ==  numRows)
         	{
-        		//check if db row exists or else break
-        			
-        		/*
-        		if (response.rows === undefined || response.rows.length == 0) 
+        		console.log("dbrow matches numRows: " + dbrow);
+        		break;
+        	}
+        	
+        	for(var j = 0; j <= 23; j++)//24 hours
+        	{
+        		if(dbrow == numRows)
         		{
-        			console.log("NO DATA")
+        			console.log("dbrow matches numRows: " + dbrow);
+        			break;
         		}
-        		*/
-        		    // array empty or does not exist
-        		
         		
         		if(response.rows[dbrow].col_num != i)//on to next column
         		{
@@ -80,97 +89,51 @@ function makeButtonGraphs(res)
         		}
         		else
         		{
-        			//`button${i}[${j}]` = response.row[dbrow].count; 
+      
         			if(i == 0)
         			{
         				button1[j] = response.rows[dbrow].count;  
         			}
-        			else if(i ==1)
+        			else if(i == 1)
         			{
         				button2[j] = response.rows[dbrow].count; 
         			}
-        			else if(i ==2)
+        			else if(i == 2)
         			{
         				button3[j] = response.rows[dbrow].count; 
         			}
-        			else if(i ==3)
+        			else if(i == 3)
         			{
         				button4[j] = response.rows[dbrow].count; 
         			}
-        			else if(i ==4)
+        			else if(i == 4)
         			{
         				button5[j] = response.rows[dbrow].count; 
         			}
-        			else if(i ==5)
+        			else if(i == 5)
         			{
         				button6[j] = response.rows[dbrow].count; 
         			}
-        			else if(i ==6)
+        			else if(i == 6)
         			{
         				button7[j] = response.rows[dbrow].count; 
         			}
-        			else if(i ==7)
+        			else if(i == 7)
         			{
-        				button8[j] = response.rows[dbrow].count; 
+        				button8[j] = response.rows[dbrow].count; 	
         			}
         			dbrow++;
         		}
         	}	
         }
-    
-        var graphNumber = 0;
-        var srcs = [];
-        
-        for(var i = 1; i<=8; i++)
-        {
-        	var graphData = [{x:hour_starts, y:`button${i}`, type: 'bar'}];
-        	var layout = {fileopt : "overwrite", filename : "Popgraph${i}"};
-
-            plotly.plot(graphData, layout, function (err, msg)
-            {
-                if (err)
-                {
-                    console.log(`Plotly plot error: ${err}`);
-                    res.end();
-                    return;
-                }    
-
-                var url = msg.url;
-                var start = url.indexOf('//');
-                var suburl = url.slice(start);	
-                var newsrc = suburl.concat(".embed");        
-                graphNumber++;
-                plotUrl = newsrc;
-                srcs.push(newsrc);
-                //console.log(srcs);
-              
-                console.log(`Sending plot with url: ${plotUrl}`);
-                if(graphNumber == 8)
-                {
-                	//console.log(srcs);
-                	res.json({src: srcs});
-                	res.end();
-            	}
-            });
-        }
-        //while(graphNumber != 8){}
-        //res.json({src: srcs});
+        res.json(buttons);
+        res.end(); 
     });
 }
 
-/*
-function plotGraph(data, )
-{
-	
-}
-*/
-
-
 function getPopTableUpdate(res)
 {
-	
-
-    var updateObj = {
+    	var updateObj = {
         button1: { count: 0},
         button2: { count: 0},
         button3: { count: 0},
@@ -186,11 +149,11 @@ function getPopTableUpdate(res)
     // Iterate through each of the 8 columns and get their count data
     for (var i = 1; i <= 8; i++)
     {
-	console.log("I value: " + i);
-	var index = i;
+    	//console.log("I value: " + i);
+    	var index = i;
         ((index) => psqlClient.query(`SELECT count FROM drinks WHERE col_num = ${i-1};`, function (err, response)
         {
-	    console.log("Index in callback: " + index);
+        	//console.log("Index in callback: " + index);
             if (err)
             {
                 console.log(`Error in database query for button ${index}\nError is: ${err}`);
@@ -198,17 +161,17 @@ function getPopTableUpdate(res)
                 return;
             }
 
-            console.log(`Query for button ${index} has response: ${JSON.stringify(response)}`);
-	    console.log(updateObj);
+            //console.log(`Query for button ${index} has response: ${JSON.stringify(response)}`);
+            //console.log(updateObj);
             updateObj[`button${index}`].count = response.rows[0].count;
             respondedQueries += 1;
 
             // When all 8 queries have been responded, send the data
             if (respondedQueries == 8)
             {
-                console.log("Sending pop table update");
+                //console.log("Sending pop table update");
                 res.json(updateObj);
-		res.end()
+                res.end()
             }
         }))(index);
     }
@@ -219,41 +182,6 @@ function getPopTableUpdate(res)
 function fileGetFunc(fileName, contentType)
 {
     return (req, res) => res.sendFile(fileName, {root: __dirname, headers: {"Content-Type": contentType}});
-}
-
-function getGraph(res)
-{
-	/*
-    var data = [{x:[0,1,2], y:[3,2,1], type: 'bar'}];
-    var layout = {fileopt : "overwrite", filename : "simple-node-example"};
-
-    plotly.plot(data, layout, function (err, msg)
-    {
-        if (err)
-        {
-            console.log(`Plotly plot error: ${err}`);
-            res.end(500);
-            return;
-        }    
-
-        var url = msg.url;
-        var start = url.indexOf('//');
-        var suburl = url.slice(start);	
-        var newsrc = suburl.concat(".embed");        
-        
-        plotUrl = newsrc;
-        console.log(`Sending plot with url: ${plotUrl}`);
-        res.json({src: plotUrl});
-    });
-    */
-	
-	
-	
-	
-	
-	
-	
-	
 }
 
 // Server routing functions
@@ -269,10 +197,9 @@ server.get("/", function (req, res)
 
 server.post("/getGraph", function (req, res)
 {
-    // We had a bunch of stupid shit to get this to work right,
-    // And then I remembered this works...
-    //getGraph(res);
-	makeButtonGraphs(res);
+	console.log("req" + req);
+	console.log("res" + res);
+	getGraphDataPoints(res);
 	
 });
 
@@ -285,5 +212,3 @@ server.listen(SERVER_PORT, function ()
 {
     console.log("Server listening on port " + SERVER_PORT);
 });
-
-//tcpServer.listen(server2);
